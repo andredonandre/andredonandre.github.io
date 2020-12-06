f287a1419ee813362546236f3746d5825bd3f6de
Andrew <andrew@renthia.com>
2020-12-06 18:50:44 +0100

    - Added angularjs to website

---
+++ app/app.js
@@ -0,0 +1,18 @@
+// Define the `phonecatApp` module
+var App = angular.module('App', []);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+phonecatApp.controller('MainController', function PhoneListController($scope) {
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+});
